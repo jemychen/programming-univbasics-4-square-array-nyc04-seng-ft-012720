2e2c4916cab1aa66bab5e81452c49e0136ad9745
Jemy Chen <jemychen@live.com>
2020-01-20 17:30:25 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+  squared = []
+  counter = 0
+    while counter < array.length
+    squared.push(array[counter]*array[counter])
+    counter += 1
+   end
+   return squared
 end
+
+p square_array([1,2,3])
+p square_array([9,10,16,25])
